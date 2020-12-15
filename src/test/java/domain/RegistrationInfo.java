715ce42c43795f2da00f9ca0e85f340014e22c51
SemykinaOlga <semikinaolga@mail.ru>
2020-12-15 17:46:20 +0300

    add project

---
+++ src/test/java/domain/RegistrationInfo.java
@@ -0,0 +1,16 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class RegistrationInfo {
+    private String city;
+    private String dateFirstMeeting;
+    private String lastName;
+    private String firstName;
+    private String phoneNumber;
+    private String dateSecondMeeting;
+}
